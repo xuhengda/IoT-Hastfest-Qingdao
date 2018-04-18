9e2e8f7c672a5f38b4a8956da6a10f770668512b
xuhengda <116625817@qq.com>
2018-04-18 15:20:34 +0800

    kcx - 挑战 1

---
+++ kcx/CreateDeviceIdentity/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace CreateDeviceIdentity
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=ParkGate.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=TzqPYndKwZ03Rw23VUT/8+deHsukeje+SlZIcMuB0fQ=";
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "tshDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+    }
+}
