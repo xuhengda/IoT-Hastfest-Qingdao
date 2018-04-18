74913c1186ecbfb48baf773e7a1e83ebaac0ad6d
lkocok <lkocok@163.com>
2018-04-18 14:22:47 +0800

    added cs

---
+++ yawei_team/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace SimulatedDevice
+{
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Devices.Client;
+    using Newtonsoft.Json;
+
+    public class Program
+    {
+        private const string IotHubUri = "yaweiIoT.azure-devices.net";//"{iot hub hostname}";
+        private const string DeviceKey = "IQnilz+dWa2rSBHXjssdTufNG/WX0rwICA2nLTKAzqM=";
+        private const string DeviceId = "myFirstDevice";
+        private const double MinTemperature = 20;
+        private const double MinHumidity = 60;
+        private static readonly Random Rand = new Random();
+        private static DeviceClient _deviceClient;
+        private static int _messageId = 1;
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            while (true)
+            {
+                var currentTemperature = MinTemperature + Rand.NextDouble() * 15;
+                var currentHumidity = MinHumidity + Rand.NextDouble() * 20;
+
+                var telemetryDataPoint = new
+                {
+                    messageId = _messageId++,
+                    deviceId = DeviceId,
+                    temperature = currentTemperature,
+                    humidity = currentHumidity,
+                    message = "设备数据："+Guid.NewGuid().ToString()
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+                await _deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                await Task.Delay(1000);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            _deviceClient = DeviceClient.Create(IotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(DeviceId, DeviceKey), TransportType.Mqtt);
+            _deviceClient.ProductInfo = "HappyPath_Simulated-CSharp";
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+        }
+    }
+}
